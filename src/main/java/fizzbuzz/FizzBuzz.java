0f6dfc68ef35f9c2bc3d85f4e9bc6d729495a6f7
Jon Bos <bos.jon@gmail.com>
2020-03-24 16:54:48 -0500

    refactor: game rules now represented with map

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,16 +1,24 @@
 package fizzbuzz;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FizzBuzz {
+	Map<Integer, String> rules = new HashMap<Integer, String>();
+	
+	public FizzBuzz() {
+		rules.put(3, "Fizz");
+		rules.put(5, "Buzz");
+	}
 
 	public String fizzBuzz(int number) {
+		
 		String result = "";
-		
-		if (number % 3 == 0) {
-			result += "Fizz";
-		}
-		
-		if (number % 5 == 0) {
-			result += "Buzz";
+
+		for (Map.Entry<Integer, String> rule: rules.entrySet()) {
+			if (number % rule.getKey() == 0) {
+				result += rule.getValue();
+			}
 		}
 		
 		if (result.isEmpty()) {
