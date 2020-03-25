4f7c46c22dea686cf90cec9e0fd20357fb954eb8
Jon Bos <bos.jon@gmail.com>
2020-03-25 15:35:59 -0500

    test: game factory returns game

--- src/main/java/fizzbuzz/GameFactory.java
+++ src/main/java/fizzbuzz/GameFactory.java
@@ -1,8 +1,37 @@
 package fizzbuzz;
 
+import java.util.ArrayList;
+
 public class GameFactory {
 
 	public static CountingGame getGame() {
 		return new CountingGame();
+	}
+
+	public static CountingGame getFizzBuzzGame() {
+		CountingGame fb = new CountingGame();
+		Rule fizz = new Rule((x) -> x % 3 == 0, "Fizz", "");
+		Rule buzz = new Rule((x) -> x % 5 == 0, "Buzz", "");
+		ArrayList<Rule> fizzBuzzRules = new ArrayList<Rule>();
+		fizzBuzzRules.add(fizz);
+		fizzBuzzRules.add(buzz);
+		fb.setRules(fizzBuzzRules);
+		return fb;
+	}
+
+	public static CountingGame getFizzBuzzPopGame() {
+		CountingGame fbp = new CountingGame();
+
+		Rule fizz = new Rule((x) -> x % 3 == 0, "Fizz", "");
+		Rule buzz = new Rule((x) -> x % 5 == 0, "Buzz", "");
+		Rule pop = new Rule((x) -> x % 7 == 0, "Pop", "");
+
+		ArrayList<Rule> fizzBuzzPopRules = new ArrayList<Rule>();
+		fizzBuzzPopRules.add(fizz);
+		fizzBuzzPopRules.add(buzz);
+		fizzBuzzPopRules.add(pop);
+
+		fbp.setRules(fizzBuzzPopRules);
+		return fbp;
 	}
 }
--- src/test/java/test/TestGameFactory.java
+++ src/test/java/test/TestGameFactory.java
@@ -2,24 +2,40 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import fizzbuzz.CountingGame;
 import fizzbuzz.GameFactory;
+import fizzbuzz.Rule;
 
 public class TestGameFactory {
 	private GameFactory factory;
 
 	@Before
 	public void setUp() throws Exception {
-		GameFactory factory = new GameFactory();
 	}
 
 	@Test
 	public void testCreatesGameWithNoRulesWhenPassedNoArgs() {
 		CountingGame game = GameFactory.getGame();
 		assertEquals(0, game.getRules().size());
+	}
+
+	@Test
+	public void testShouldCreateFizzBuzzGameWithTwoRules() {
+		CountingGame fizzBuzz = GameFactory.getFizzBuzzGame();
+		assertEquals(fizzBuzz.getRules().size(), 2);
+	}
+
+	@Test
+	public void testShouldCreateFizzBuzzPopGameWithThreeRules() {
+		CountingGame fizzBuzzPop = GameFactory.getFizzBuzzPopGame();
+		assertEquals(fizzBuzzPop.getRules().size(), 3);
 	}
 
 }
